04f88b4fa16bf1df7fa7ff735ebe1f059bbaf3ef
Admin <admin@AppCraft-MacBook-Pro.local>
2018-10-31 11:16:20 +0300

    add config, tweak code_formatter.config, add renderer snapshot test

--- CollageMakerTests/UnitTests/CollageRendererTests.swift
+++ CollageMakerTests/UnitTests/CollageRendererTests.swift
@@ -3,18 +3,18 @@
 //
 
 @testable import CollageMaker
-import FBSnapshotTestCase
 import Photos
 import UIKit
+import XCTest
 
-class CollageRendererTests: FBSnapshotTestCase {
+class CollageRendererTests: XCTestCase {
     var collage: Collage!
     var renderer: CollageRenderer!
     var templateProvider: CollageTemplateProvider!
 
     override func setUp() {
         super.setUp()
-        recordMode = false
+
         let assets = Array(1 ... 9).map { _ in PHAsset() }
 
         templateProvider = CollageTemplateProvider(photoLibrary: MockPhotoLibrary(quality: .high))
@@ -38,13 +38,8 @@ class CollageRendererTests: FBSnapshotTestCase {
     }
 
     func testRendererPerformance() {
-        var view: UIImageView?
-
         measure {
-            let img = renderer.renderImage(from: collage, with: CGSize(width: 1300, height: 1300), borders: true)
-            view = UIImageView(image: img)
+            _ = renderer.renderImage(from: collage, with: CGSize(width: 1300, height: 1300), borders: true)
         }
-
-        FBSnapshotVerifyView(view!)
     }
 }
